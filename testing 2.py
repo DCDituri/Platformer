f966c90cd8b6a9d2f41024e4878e3871d32a5844
jennnguyen <jennnguyen@csumb.edu>
2016-05-04 14:05:54 -0700

    Revert "Adding songs to a list"
    This reverts commit 5c3594ce47bac8dcdffd8a7f06c97292de7be771.

--- testing 2.py
+++ testing 2.py
@@ -1,61 +1,33 @@
-import wave, os, sys, pygame
+import wave
 from pygame import mixer
-from pygame.locals import *
+import os
 
+""""
 folder = os.path.dirname(os.path.realpath('__file__'))
-fileName = os.path.join(folder, '..Platformer/Sounds/')
+fileName = os.path.join(folder, '../Testing/Sounds/')
 fileName = os.path.abspath(os.path.realpath(fileName))
-namesList = ["chicken", "duck", "cat", "sheep"] #name of the song files
-songList = [] #puts the song paths into a list
-for names in namesList:
-    songPath = fileName + "\\" + names + ".wav"
-    songList.append(songPath)
-print songList
-
-finalSongList = []
-
-pygame.init()
-sound = pygame.mixer.Sound(songList[1])
-
-while True:
-
-        for event in pygame.event.get():
-
-            #Changes the moving variables only when the key is being pressed
-            if event.type == KEYDOWN:
-                pygame.mixer.music.play()
-                if event.key == K_LEFT:
-                    sound.play()
-                if event.key == K_RIGHT:
-                    sound.play()
-                if event.key == K_DOWN:
-                    sound.play()
-                if event.key == K_UP:
-                    sound.play()
-
-
-            #Stops moving the image once the key isn't being pressed
-            elif event.type == KEYUP:
-                pygame.mixer.music.stop()
-                
-
+songPath = fileName + "\\" + ____ + ".wav"
 """
-for num in range(0,4):
-    sound = pygame.mixer.Sound(songList[num])
-    sound.play()
-"""
+ 
+chicken = 'C://Users//Jennifer Nguyen//Desktop//Testing//Sounds//chicken.wav'
+duck = 'C://Users//Jennifer Nguyen//Desktop//Testing//Sounds//duck.wav'
+destination = open('newSong.wav', 'wb')
+infiles = [chicken, duck]
+outfile = destination
 
-"""
+data= []
+for infile in infiles:
+    w = wave.open(infile, 'rb')
+    data.append( [w.getparams(), w.readframes(w.getnframes())] )
+    w.close()
 
-newSong = 'C:\\Users\\Jennifer Nguyen\\Documents\\GitHub\\Platformer\\Sounds\\newSong.wav'
-output = wave.open(newSong, 'wb')
+output = wave.open((fileName + "\\" + ____ + ".wav"), 'wb')
 output.setparams(data[0][0])
 output.writeframes(data[0][1])
 output.writeframes(data[1][1])
 
 mixer.init()
 
-mixer.music.load(newSong)
+mixer.music.load(output)
 mixer.music.play()
 output.close()
-"""
